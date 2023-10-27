9fdcb6afd0d3969994663c872d157a77e4015450
Mark <fqchenxin@gmail.com>
2023-10-27 14:40:48 +1100

    add bootstrap form

--- AIrDemo/AIrDemo/ClientApp/src/components/IdentifyIndividual.js
+++ AIrDemo/AIrDemo/ClientApp/src/components/IdentifyIndividual.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Spinner } from 'reactstrap';
+import { 
+  Form,
+  Input,
+  Label,
+  Spinner,
+  FormText,
+  FormGroup,
+  FormFeedback
+} from 'reactstrap';
 
 function IdentifyIndividual() {
   const [individualDetails, setIndividualDetails] = useState(null);
@@ -53,6 +61,85 @@ function IdentifyIndividual() {
   return (
     <div>
       <h1>Individual Details</h1>
+
+      <Form>
+        <FormGroup>
+          <Label for="exampleEmail">
+            Input without validation
+          </Label>
+          <Input />
+          <FormFeedback>
+            You will not be able to see this
+          </FormFeedback>
+          <FormText>
+            Example help text that remains unchanged.
+          </FormText>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleEmail">
+            Valid input
+          </Label>
+          <Input valid />
+          <FormFeedback valid>
+            Sweet! that name is available
+          </FormFeedback>
+          <FormText>
+            Example help text that remains unchanged.
+          </FormText>
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">
+            Invalid input
+          </Label>
+          <Input invalid />
+          <FormFeedback>
+            Oh noes! that name is already taken
+          </FormFeedback>
+          <FormText>
+            Example help text that remains unchanged.
+          </FormText>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleEmail">
+            Input without validation
+          </Label>
+          <Input />
+          <FormFeedback tooltip>
+            You will not be able to see this
+          </FormFeedback>
+          <FormText>
+            Example help text that remains unchanged.
+          </FormText>
+        </FormGroup>
+        <FormGroup className="position-relative">
+          <Label for="exampleEmail">
+            Valid input
+          </Label>
+          <Input valid />
+          <FormFeedback
+            tooltip
+            valid
+          >
+            Sweet! that name is available
+          </FormFeedback>
+          <FormText>
+            Example help text that remains unchanged.
+          </FormText>
+        </FormGroup>
+        <FormGroup className="position-relative">
+          <Label for="examplePassword">
+            Invalid input
+          </Label>
+          <Input invalid />
+          <FormFeedback tooltip>
+            Oh noes! that name is already taken
+          </FormFeedback>
+          <FormText>
+            Example help text that remains unchanged.
+          </FormText>
+        </FormGroup>
+      </Form>
+
       <button type="button" className="btn btn-primary" onClick={handleFetchBtnClick}>Fetch Data</button>
       {loading ? (<div style={{display: 'flex', justifyContent: 'center'}}><Spinner>Loading...</Spinner></div>) : <IndividualDetails data={individualDetails}/>}
     </div>        
