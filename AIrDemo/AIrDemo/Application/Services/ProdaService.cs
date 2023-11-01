356b29f4a99d2cdd2e7c963be4927a277c04f59c
Swinkaran <swinkaran@hotmail.com>
2023-11-02 10:03:48 +1100

    Proda auth with jks file
    Proda auth with jks file

--- AIrDemo/AIrDemo/Application/Services/ProdaService.cs
+++ AIrDemo/AIrDemo/Application/Services/ProdaService.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json;
-using System.Text;
+﻿using System.Security.Cryptography.X509Certificates;
 
 namespace AIrDemo.Application.Services
 {
@@ -10,8 +9,11 @@ namespace AIrDemo.Application.Services
             try
             {
                 //var httpClient = _httpClientFactory.CreateClient("AirServiceApi");
+                var handler = new HttpClientHandler();
+                var cert = new X509Certificate2("<pk12_file_name.p12>", "<password>");
+                handler.ClientCertificates.Add(cert);
 
-                HttpClient httpClient = new HttpClient();
+                HttpClient httpClient = new HttpClient(handler);
 
                 httpClient.BaseAddress = new Uri("https://test.healthclaiming.api.humanservices.gov.au/");
                 httpClient.DefaultRequestHeaders.Add("X-IBM-Client-Id", "27e9de6e5d2499e424f2a7394258541b");
@@ -23,9 +25,7 @@ namespace AIrDemo.Application.Services
                 httpClient.DefaultRequestHeaders.Add("dhs-productId", "RsvpAir 2.0");
                 httpClient.DefaultRequestHeaders.Add("dhs-subjectIdType", "Date Of Birth");
                 httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "eyJraWQiOiI2dzBZTGF2QUFDYjFMbUVvb1hlTmUwaUoxUmpEdkFUWW5FZG1qMmNDbmtjIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiIxMjg0NDY3NjY2IiwiYXVkIjoiUFJPREEuVU5BVFRFTkRFRC5CMkIiLCJwcm9kYS5zd2luc3QiOiJUZXN0UnN2cENvbm4iLCJwcm9kYS50eXBlIjoiVU5BVFRFTkRFRC5CMkIiLCJwcm9kYS5vcmciOiIxMjg0NDY3NjY2IiwicHJvZGEucnAiOiJNQ09MIiwicHJvZGEuc3AiOlsiTUNPTCJdLCJwcm9kYS5hdWQiOiJodHRwczovL21lZGljYXJlYXVzdHJhbGlhLmdvdi5hdS9NQ09MIiwiaXNzIjoiaHR0cHM6Ly9wcm9kYS5odW1hbnNlcnZpY2VzLmdvdi5hdSIsImlhdCI6MTY5NjM4MzI0NSwiZXhwIjoxNjk2Mzg2ODQ1fQ.YdQAqhT4fQYFueXVvzIwtVUJ5HQVispVK5WTrekWrxy-ePmOyxsgLsnB_sshtYIcFvMy9cHGNAEzh9DDCKEx20lKeAzmP9iAsRo2xrlDztG3zXr0d-LD2QC6-CZdQHLEshg2AsPDu-VWb8W4pwbIaymEK831_rT_ucZBdkojzD2rMucTh1w28O8whzan2HRMohFh9EQpUoiinynqmKYCHkzn0eB_GDLjTZzsEzjIhic9QECWfdRLnh6B_CHmd-qkIiuT0iDvJ128RhYlGfeGGsdOAEXr7dgfMIS_BYbZnepGXC4jDxx3M8pOysMstVQzwj1tCMv-xq8wepBlJ9zs7A");
-
-
-                
+        
                 var serializeOptions = new JsonSerializerOptions
                 {
                     PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
